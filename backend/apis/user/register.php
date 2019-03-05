6fdc5f88b16c09a5cc4511580e1df3ae6eeebcbe
basel <basel.3ly@gmail.com>
2019-03-05 13:45:50 +0200

    edit

--- backend/apis/user/register.php
+++ backend/apis/user/register.php
@@ -1,4 +1,6 @@
 <?php
+require '../../vendor/autoload.php';
+
 header("Content-Type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Origin: http://localhost:8080');
 header('Access-Control-Allow-Headers: content-type');
